28501ef131e1c7e894e254fdbaa900607d497a59
Gleb <ershov_g@auca.kg>
2017-01-06 14:42:51 +0600

    Create toggle() and delete() functions

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,4 +16,16 @@ const todos = [
 export class AppComponent {
     title = 'Angular 2Do'
     todos = todos;
+
+    toggle(todo: any) {
+        todo.completed = !todo.completed;
+    }
+
+    delete(todo: any) {
+        let index = this.todos.indexOf(todo);
+
+        if(index > -1){
+            this.todos.splice(index, 1);
+        }
+    }
 }
